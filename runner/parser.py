7186f0753234bb29a4271e740924fc6da222a93f
Alex <alexcros@mtu.edu>
2026-09-20 19:02:50 -0400

    feat: parse hem state and compare against expected
    Signed-off-by: Alex <alexcros@mtu.edu>

--- runner/parser.py
+++ runner/parser.py
@@ -1,6 +1,11 @@
 import tomllib
+import json
 from pathlib import Path
 
 def load_test(path: Path) -> dict:
     with path.open("rb") as f:
         return tomllib.load(f)
+
+def load_state(path: Path):
+    with path.open("r", encoding="utf-8") as f:
+        return json.load(f)
--- runner/runner.py
+++ runner/runner.py
@@ -2,7 +2,7 @@ import subprocess
 import tempfile
 from pathlib import Path
 
-from .parser import load_test
+from .parser import load_state, load_test
 
 def run_test(path: Path) -> bool:
     test = load_test(path)
@@ -14,23 +14,39 @@ def run_test(path: Path) -> bool:
 
         asm_file = tmp / "program.hasm"
         binary_file = tmp / "program"
-        state_file = tmp / "state.json" # TODO: Implement cJSON in hem
+        state_file = tmp / "state.json"
 
         asm_file.write_text(
                 "\n".join(test["program"]) + "\n"
         )
 
         subprocess.run(
-            ["hasm", str(asm_file), "-o", str(binary_file)],
+            ["./hasm", str(asm_file), "-o", str(binary_file)],
             check=True
         )
 
         subprocess.run(
-            ["hem", str(binary_file), "--dump-state", str(state_file)],
+            ["./hem", str(binary_file), "--dump-state", str(state_file)],
             check=True
         )
 
+        registers = test["expect"]["registers"]
+        valid = True
 
-    print("PASS")
+        state = load_state(state_file)
+
+        for register, expected in registers.items():
+            index = int(register[1:])
+
+            actual = state["registers"][index]
+
+            if actual != expected:
+                print(
+                        f"FAIL: {register}: "
+                        f"expected {expected}, got {actual}"
+                )
+                valid = False
+            else:
+                print("PASS")
 
     return True
